b074d96e86817dc7914fbcba799e3934409ea6ef
Thomas Iversen Ramm <thomas.ramm@hotmail.com>
2021-01-30 18:31:49 +0100

    Changed bacground color to purple

--- core/src/com/mygdx/game/FirstGdxGame.java
+++ core/src/com/mygdx/game/FirstGdxGame.java
@@ -27,7 +27,7 @@ public class FirstGdxGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 0, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		// Change direction
