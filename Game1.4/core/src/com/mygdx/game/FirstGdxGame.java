edc7bbcead1ea7dc6725c266b7bf80492d41fa80
Vilde Almestad <vildealmestad@dhcp-10-220-212-110.wlan.ntnu.no>
2021-02-01 16:25:47 +0100

    finishing touches

--- Game1.4/core/src/com/mygdx/game/FirstGdxGame.java
+++ Game1.4/core/src/com/mygdx/game/FirstGdxGame.java
@@ -8,19 +8,16 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Vector3;
+
 
 
 public class FirstGdxGame extends ApplicationAdapter {
 	private SpriteBatch batch;
-	private Texture recTextL, recTextR,ballText;
-	private Sprite paddleLeft, paddleRight,ball;
-	private Vector2 leftPos, rightPos;
-	private Vector3 ballPos;
-	private float xSpeed = 4f;
-	private float ySpeed = 4f;
-	private float paddleSpeed = 4f;
+	private Texture recTextL, recTextR,ballText, background;
+	private Sprite paddleLeft, paddleRight,ball, backgroundSprite;
+	private float xSpeed = 6f;
+	private float ySpeed = 6f;
+	private float paddleSpeed = 10f;
 	private Rectangle recL, recR, recB;
 	private int score1,score2 = 0;
 	private BitmapFont font;
@@ -33,80 +30,88 @@ public class FirstGdxGame extends ApplicationAdapter {
 		recTextL = new Texture("paddle1.png");
 		recTextR = new Texture("paddle1.png");
 		ballText = new Texture("ball.png");
+		background = new Texture("background.png");
 		paddleRight = new Sprite(recTextR);
 		paddleLeft = new Sprite(recTextL);
 		ball = new Sprite(ballText);
-		leftPos = new Vector2(paddleRight.getWidth(), Gdx.graphics.getHeight()/2);
-		rightPos = new Vector2(Gdx.graphics.getWidth()-30, Gdx.graphics.getHeight()/2);
-		ballPos = new Vector3(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2,0);
+		backgroundSprite = new Sprite(background);
+
+		recL = new Rectangle(0,Gdx.graphics.getHeight()/2,50, 250);
+		recR = new Rectangle(Gdx.graphics.getWidth()-paddleRight.getWidth()/2, Gdx.graphics.getHeight()/2, 50,250);
+		recB = new Rectangle(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2, 30, 30);
 	}
 
 	public boolean IsInsideScreen(float x, float y){
-		if(leftPos.x ==  paddleRight.getWidth() &&
-				leftPos.y< Gdx.graphics.getHeight()-paddleLeft.getHeight()/2 || leftPos.y > 0+paddleLeft.getHeight()/2) {
+		if(recL.y < Gdx.graphics.getHeight() || recL.y > 0) {
 			return true;
 		}
 		return false;
 	}
 
+	public void startOver(){
+		recB.x = Gdx.graphics.getWidth()/2;
+		recB.y = Gdx.graphics.getHeight()/2;
+	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		if (Gdx.input.isTouched() && IsInsideScreen(leftPos.x, leftPos.y)) {
-			leftPos.x = paddleRight.getWidth();
-			leftPos.y = Gdx.graphics.getHeight()-Gdx.input.getY();
+		if (Gdx.input.isTouched() && IsInsideScreen(recL.x, recL.y)) {
 
-			if (leftPos.x > paddleRight.getWidth() || leftPos.x < paddleRight.getWidth() ) {
-				leftPos.x = paddleRight.getWidth();
+			recL.y = Gdx.graphics.getHeight()-Gdx.input.getY();
+
+			if (recL.x > paddleRight.getWidth() || recL.x < paddleRight.getWidth() ) {
+				recL.x = 0;
 			}
-			if (leftPos.y > Gdx.graphics.getHeight() - paddleLeft.getHeight() / 2) {
-				leftPos.y = Gdx.graphics.getHeight() - paddleLeft.getHeight()/2;
+			if (recL.y > Gdx.graphics.getHeight() - paddleLeft.getHeight() / 2) {
+				recL.y = Gdx.graphics.getHeight() - paddleLeft.getHeight()/2;
 			}
-			if (leftPos.y < 0+ paddleLeft.getHeight()/2) {
-				leftPos.y = 0 + paddleLeft.getHeight()/2;
+			if (recL.y < 0) {
+				recL.y = 0;
 			}
 		}
 
-		if(rightPos.x > Gdx.graphics.getHeight()-paddleRight.getHeight()/2){
-			score2 += 1;
-		}
-
-		if(rightPos.y > Gdx.graphics.getHeight() - paddleRight.getHeight()/2 || rightPos.y - paddleRight.getHeight()/2 < 0){
+		if(recR.y > Gdx.graphics.getHeight()-recR.getHeight()|| recR.y < 0){
 			paddleSpeed = -paddleSpeed;
 		}
 
-		if(ballPos.y > Gdx.graphics.getHeight() - ball.getHeight()/2 || ballPos.y - ball.getHeight()/2 < 0){
+		if(recB.y > Gdx.graphics.getHeight() - ball.getHeight()/2 || recB.y - ball.getHeight()/2 < 0){
 			ySpeed = -ySpeed;
 		}
-
-		ballPos.y += ySpeed;
-		ballPos.x += xSpeed;
-		rightPos.y += paddleSpeed;
-
-
-		recL = new Rectangle(leftPos.x,leftPos.y,paddleLeft.getWidth(), paddleLeft.getHeight());
-		recR = new Rectangle(rightPos.x, rightPos.y, paddleRight.getWidth(),paddleRight.getHeight());
-		recB = new Rectangle(ballPos.x, ballPos.y, ball.getWidth(), ball.getHeight());
-
-		if(recB.overlaps(recR) || ballPos.x < Gdx.graphics.getWidth() + recR.getWidth()/2){
-			xSpeed = -xSpeed;
-			ySpeed = -ySpeed;
+		if(recB.x > Gdx.graphics.getWidth()){
+			startOver();
+			score1 += 1;
+		}
+		if(recB.x - ball.getWidth()/2 < - 50){
+			startOver();
 			score2 += 1;
 		}
-		if(recB.overlaps(recL) || ballPos.x > 0 + recL.getWidth()/2){
+
+		recB.y += ySpeed;
+		recB.x += xSpeed;
+		recR.y += paddleSpeed;
+
+
+		if(recB.overlaps(recR) || recB.overlaps(recL)){
 			xSpeed = -xSpeed;
-			ySpeed = -ySpeed;
-			score1 +=1;
 		}
 
+
 		batch.begin();
-		batch.draw(paddleLeft, leftPos.x - paddleLeft.getWidth() / 2,leftPos.y - paddleLeft.getHeight() / 2);
-		batch.draw(paddleRight, rightPos.x - paddleRight.getWidth() / 2,rightPos.y - paddleRight.getHeight() / 2);
-		batch.draw(ball, ballPos.x - ball.getWidth() / 2,ballPos.y - ball.getHeight() / 2);
+		batch.draw(paddleLeft, recL.x,recL.y, 50, 250);
+		batch.draw(paddleRight, recR.x,recR.y, 50, 250);
+		batch.draw(ball, recB.x,recB.y, 30, 30);
 		font.draw(batch, "Score: " + "player1: " + score1+ " player2: " + score2, Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()-40);
+		if(score1 > 5){
+			batch.draw(backgroundSprite, 0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+			font.draw(batch, "You Win", Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2 );
+		}
+		if(score2 > 5){
+			batch.draw(backgroundSprite, 0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+			font.draw(batch, "You Lose", Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2 );
+		}
 		font.getData().setScale(4, 4);
 		batch.end();
 	}
